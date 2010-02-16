e63e8781cde927b716cf20f38a3ca4bd7c53b335
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-02-16 21:48:12 +0100

    The bottom box now will retain it's minimum size

--- SplitViewDelegate.rb
+++ SplitViewDelegate.rb
@@ -2,15 +2,13 @@ class SplitViewDelegate
   attr_accessor :split_view
 
   MIN_SIZE = 200
-  MAX_SIZE = 100 # height - this
+  MAX_SIZE = 148 # height - this
 
   def	splitView(sender, constrainMinCoordinate: proposed_coordinate, ofSubviewAt: index)
-    puts "min"
     proposed_coordinate + MIN_SIZE
   end
 
   def	splitView(sender, constrainMaxCoordinate: proposed_coordinate, ofSubviewAt: index)
-    puts "max"
     proposed_coordinate - MAX_SIZE
   end
 
@@ -21,13 +19,19 @@ class SplitViewDelegate
     bottom = sender.subviews.objectAtIndex(1)
     bottom_frame = bottom.frame
     divider_thickness = sender.dividerThickness
-    
+
     top_frame.size.width = new_frame.size.width
-    
+
     bottom_frame.size.height = new_frame.size.height - top_frame.size.height - divider_thickness
     bottom_frame.size.width = new_frame.size.width
     bottom_frame.origin.y = top_frame.size.height + divider_thickness
-    
+
+    if bottom_frame.size.height < MAX_SIZE
+      top_frame.size.height = top_frame.size.height - (MAX_SIZE - bottom_frame.size.height) - 2
+      bottom_frame.size.height = MAX_SIZE
+    end
+    # There are problems when only resizing vertical with this. redraw doesn't work very well.
+
     top.frame = top_frame
     bottom.frame = bottom_frame
   end
