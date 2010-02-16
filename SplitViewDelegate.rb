ad202efcdabdd5c3292ff9934ccd9a423de037a6
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-02-16 21:24:27 +0100

    Fixed weird behaviour with the split view. The top bottom box will remain its size when resizing the window

--- SplitViewDelegate.rb
+++ SplitViewDelegate.rb
@@ -4,11 +4,31 @@ class SplitViewDelegate
   MIN_SIZE = 200
   MAX_SIZE = 100 # height - this
 
-  def	splitView(split_view, constrainMinCoordinate: proposed_coordinate, ofSubviewAt: index)
+  def	splitView(sender, constrainMinCoordinate: proposed_coordinate, ofSubviewAt: index)
+    puts "min"
     proposed_coordinate + MIN_SIZE
   end
 
-  def	splitView(split_view, constrainMaxCoordinate: proposed_coordinate, ofSubviewAt: index)
+  def	splitView(sender, constrainMaxCoordinate: proposed_coordinate, ofSubviewAt: index)
+    puts "max"
     proposed_coordinate - MAX_SIZE
+  end
+
+  def splitView(sender, resizeSubviewsWithOldSize: old_size)
+    new_frame = sender.frame
+    top = sender.subviews.objectAtIndex(0)
+    top_frame = top.frame
+    bottom = sender.subviews.objectAtIndex(1)
+    bottom_frame = bottom.frame
+    divider_thickness = sender.dividerThickness
+    
+    top_frame.size.width = new_frame.size.width
+    
+    bottom_frame.size.height = new_frame.size.height - top_frame.size.height - divider_thickness
+    bottom_frame.size.width = new_frame.size.width
+    bottom_frame.origin.y = top_frame.size.height + divider_thickness
+    
+    top.frame = top_frame
+    bottom.frame = bottom_frame
   end
 end
