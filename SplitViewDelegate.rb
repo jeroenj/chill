ad34ddd3d793ea25c418e39780a32f67e794847a
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-02-19 10:13:29 +0100

    Fixed SplitView where it would go out of bounds and throw nasty warnings in the console

--- SplitViewDelegate.rb
+++ SplitViewDelegate.rb
@@ -13,24 +13,24 @@ class SplitViewDelegate
   end
 
   def splitView(sender, resizeSubviewsWithOldSize: old_size)
-    new_frame = sender.frame
     top = sender.subviews.objectAtIndex(0)
-    top_frame = top.frame
     bottom = sender.subviews.objectAtIndex(1)
+
+    new_frame = sender.frame
+    top_frame = top.frame
     bottom_frame = bottom.frame
+
     divider_thickness = sender.dividerThickness
 
-    top_frame.size.width = new_frame.size.width
+    top_frame.size.width = bottom_frame.size.width = new_frame.size.width
 
     bottom_frame.size.height = new_frame.size.height - top_frame.size.height - divider_thickness
-    bottom_frame.size.width = new_frame.size.width
-    bottom_frame.origin.y = top_frame.size.height + divider_thickness
-
     if bottom_frame.size.height < MAX_SIZE
-      top_frame.size.height = top_frame.size.height - (MAX_SIZE - bottom_frame.size.height) - 2
+      top_frame.size.height = top_frame.size.height - (MAX_SIZE - bottom_frame.size.height)
       bottom_frame.size.height = MAX_SIZE
     end
-    # There are problems when only resizing vertical with this. redraw doesn't work very well.
+
+    bottom_frame.origin.y = top_frame.size.height + divider_thickness
 
     top.frame = top_frame
     bottom.frame = bottom_frame
